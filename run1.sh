857c2ce5132d67b07a171ecda16511d9b42b4348
koltona <droyktton@gmail.com>
2025-04-28 16:45:05 -0300

    program update

---
+++ run1.sh
@@ -0,0 +1,9 @@
+## Runs many seed for one field
+
+for((seed=1234;seed<=1234+20;seed++))
+do 
+	echo "seed="$seed; 
+	./spatialuphi 65536 1.0005 1.0005 1.01 1000000 $seed; 
+	mv inst_sofq.dat "inst_sofq"$seed".dat"; 
+	mv logcm.dat "logcm"$seed".dat"; 
+done
